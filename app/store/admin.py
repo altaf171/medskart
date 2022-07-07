cbe022837f821fc4e94348646ad9755a157f552a
altaf171 <altaf78@outlook.com>
2022-07-07 15:50:30 +0530

    added stock, productdetails to serializer

--- app/store/admin.py
+++ app/store/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from store.models import Drug, Prescription
+from store.models import (
+    Drug,
+    Prescription,
+    Stock
+)
 
 admin.site.register(Drug)
-
 admin.site.register(Prescription)
+admin.site.register(Stock)
--- app/store/api/serializers.py
+++ app/store/api/serializers.py
@@ -1,5 +1,4 @@
 
-from pyexpat import model
 from rest_framework import serializers
 from .utils import serializer_utils
 from store.models import (
@@ -7,11 +6,28 @@ from store.models import (
     Prescription,
     Drug,
     DrugImage,
+    ProductDetails,
+    Stock,
 )
 
 
+class ProductDetailsModelSeializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductDetails
+        fields = '__all__'
+
+class StockModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+
+
+
+
 class DrugImageModelSerializer(serializers.ModelSerializer):
-    # image_url = serializers.SerializerMethodField()
+    """
+    Serializes DrugImage model gives image's url
+
+    """
     image = serializers.ImageField(
         max_length=None, use_url=True, allow_null=True, required=False)
 
@@ -21,6 +37,12 @@ class DrugImageModelSerializer(serializers.ModelSerializer):
 
 
 class DrugModelSerializer(serializers.ModelSerializer):
+    """
+    
+    Serializes Drug model and give all info about medicine
+
+    """
+
     url = serializer_utils.ParameterisedHyperlinkedIdentityField(
         view_name="drug-detail",
         lookup_fields=(('prescription.slug', 'prescription_slug'),
@@ -36,13 +58,62 @@ class DrugModelSerializer(serializers.ModelSerializer):
 
     compounds = serializers.StringRelatedField(many=True)
 
+    details  =ProductDetailsModelSeializer(source='productdetails')
+
+    in_stock = serializers.SerializerMethodField(method_name="get_stock")
+
     class Meta:
         model = Drug
-        fields = ['url', 'images', 'name', 'compounds' ]
+        fields = ['url', 'images', 'name', 'compounds', 'details', 'in_stock']
+    
+    def get_stock(self, obj):
+        """
+        calculate that is item in stock or not
+
+        """
+        items=0
+        if obj.stock_set.all():
+            for stock in obj.stock_set.all():
+                items += stock.items
+        
+        if items:
+            return True
+        return False
+
+        # return {"items": items}
+
+
+
+class DrugInlineModelSerializer(serializers.ModelSerializer):
+    """
+    This Serializer is used inside PrescriptionModelSerializer
+    to display limited info of drug
+
+    """
+    url = serializer_utils.ParameterisedHyperlinkedIdentityField(
+        view_name="drug-detail",
+        lookup_fields=(('prescription.slug', 'prescription_slug'),
+                       ('slug', 'medicine_slug')),
+
+    )
+
+    images = DrugImageModelSerializer(
+        source='drugimage_set',
+        many=True,
+        read_only=True,
+    )
+
+
+    compounds = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Drug
+        fields = ['url', 'images', 'name', 'compounds']
+
 
 
 class PrescriptionModelSerializer(serializers.ModelSerializer):
-    medicines = DrugModelSerializer(
+    medicines = DrugInlineModelSerializer(
         source='drug_set', many=True, read_only=True)
 
     url = serializer_utils.ParameterisedHyperlinkedIdentityField(
