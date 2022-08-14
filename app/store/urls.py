42124c312eb88d4d68f060f2714eec799c7ce376
altaf171 <altaf78@outlook.com>
2022-08-14 21:31:21 +0530

    header is finished

--- app/store/urls.py
+++ app/store/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import (
     homePage
@@ -6,5 +7,6 @@ from .views import (
 
 urlpatterns = [
     path('',homePage, name="home-page"),
+    path('login/', auth_views.LoginView.as_view(template_name="store/login.html"), name="login")
     
 ]
